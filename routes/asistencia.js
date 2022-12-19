bd6b0928be0666e5ac496af07bc806b6b0ab3975
GaryFernandoYM <miraclevann224@gmail.com>
2022-12-18 19:28:52 -0500

    registro Usuario

--- routes/asistencia.js
+++ routes/asistencia.js
@@ -2,18 +2,18 @@ var express = require('express');
 var router = express.Router();
 var dbConn  = require('../lib/db');
  
-// router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) {
       
-//     dbConn.query('SELECT * FROM registro_asistencia ORDER BY reg_id asc',function(err,rows)     {
+     dbConn.query('SELECT * FROM registro_asistencia ORDER BY reg_id asc',function(err,rows)     {
  
-//         if(err) {
-//             req.flash('error', err);
-//             // render to views/asistencia/index.ejs
-//             res.render('asistencia',{data:''});   
-//         } else {
-//             // render to views/asistencia/index.ejs
-//             res.render('asistencia',{data:rows});
-//         }
-//     });
-// });
+         if(err) {
+             req.flash('error', err);
+             // render to views/asistencia/index.ejs
+             res.render('asistencia',{data:''});   
+         } else {
+             // render to views/asistencia/index.ejs
+             res.render('asistencia',{data:rows});
+        }
+   });
+});
 module.exports = router;
